db8e84e2f2189c55a6199e33c192e97a380eeeb8
thurendous <thurendous2@gmail.com>
2022-09-04 21:33:25 +0900

    Deploy website - based on dc8fb7ec08f6b91a447b135706c677c482a4c085

---
+++ en/assets/js/2de2fa26.e29976d7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website_with_docusaurus=self.webpackChunkmy_website_with_docusaurus||[]).push([[4248],{7022:e=>{e.exports=JSON.parse('{"label":"event","permalink":"/en/docs/tags/event","allTagsPath":"/en/docs/tags","count":1,"items":[{"id":"Learning/Event","title":"20 Event","description":"solidity event","permalink":"/en/docs/Learning/Event"}]}')}}]);
