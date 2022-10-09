2cf279e1e91365084400cdf1961f9c254aa1b001
thurendous <thurendous2@gmail.com>
2022-10-09 16:20:57 +0900

    minor fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ function HomepageHeader() {
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="'/docs/category/ethersjs"
+                        to="/docs/category/ethersjs"
                     >
                         Ethers.js
                     </Link>
