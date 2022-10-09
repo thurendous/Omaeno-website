b5e3ed0fe085b105010d353ad428a563dc662456
thurendous <thurendous2@gmail.com>
2022-10-09 15:55:44 +0900

    minor fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ function HomepageHeader() {
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="/etherWeiExchange"
+                        to="/unitConverter"
                     >
                         Tool - Unit Converter
                     </Link>
